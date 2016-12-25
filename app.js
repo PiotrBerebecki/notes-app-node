d40f9340f3a43c0a32b455fc10747ffcafe60585
PiotrBerebecki <petebere@gmail.com>
2016-12-25 21:28:12 +0700

    Add yargs to process arguments

---
+++ app.js
@@ -0,0 +1,28 @@
+console.log('Starting the app!');
+
+var fs = require('fs');
+const _ = require('lodash');
+const yargs = require('yargs');
+
+var notes = require('./notes.js');
+
+const argv = yargs.argv;
+var command = argv._[0];
+console.log('Command', command);
+// process log in not user friendly so
+// we will be using yargs instead
+// console.log('process', process.argv);
+console.log('yargs', argv);
+
+if (command === 'add') {
+  notes.addNote(argv.title, argv.body);
+  
+} else if (command === 'list') {
+  notes.getAll();
+} else if (command === 'read') {
+  notes.getNote(argv.title);
+} else if (command === 'remove') {
+  notes.removeNote(argv.title);
+} else {
+  console.log('Command not recognised');
+}
