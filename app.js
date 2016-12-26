1633e7d9fecbf2b9470d58c0de51c2c1d64ce71c
PiotrBerebecki <petebere@gmail.com>
2016-12-26 07:51:18 +0700

    Add getAll function

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
-console.log('Starting the app!');
-
 var fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
 
 var notes = require('./notes.js');
 
-const argv = yargs.argv;
-var command = argv._[0];
-console.log('Command', command);
-// process log in not user friendly so
+// In-build process log in not user friendly so
 // we will be using yargs instead
 // console.log('process', process.argv);
-console.log('yargs', argv);
+const argv = yargs.argv;
+var command = argv._[0];
 
 // Add a note
 if (command === 'add') {
@@ -26,7 +22,9 @@ if (command === 'add') {
   
 // List notes
 } else if (command === 'list') {
-  notes.getAll();
+  var allNotes = notes.getAll();
+  console.log(`Printing ${allNotes.length} note(s)`);
+  allNotes.forEach(note => notes.logNote(note));
   
 // Show body of a selected note
 } else if (command === 'read') {
@@ -44,7 +42,7 @@ if (command === 'add') {
   let message = wasNoteRemoved ? 'Note was removed' : 'Note not found';
   console.log(message);
   
-// Handle unrecognised commnads
+// Handle unrecognised commands
 } else {
   console.log('Command not recognised');
 }
